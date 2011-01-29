6c99a6b0ede2ded8b5a99ccfeefcb5e8239d3a3b
Vincent Web <vincentweb984@hotmail.com>
2011-01-29 16:26:09 +0100

    backup

---
+++ debug.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "debug.h"
+
+void debugHex(const char* buffer, int end=0)
+{
+    if (end == 0)
+        end = sizeof(buffer);
+    for(int i=0 ; i < end ; i++)
+    {
+        if (i%16 == 0)
+            printf("\n");
+        printf("%x ",(unsigned char)buffer[i]); // with signed char we would get -127 instead of 129 for example
+    }
+    printf("\n");
+}
+
+void debugDec(const char* buffer, unsigned int end=0)
+{
+    for(unsigned int i=0 ; i < end ; i++)
+    {
+        printf("%d ",(unsigned char)buffer[i]);
+    }
+    printf("\n");
+}
+
+void debugChar(const char* buffer, unsigned int end=0)
+{
+    for(unsigned int i=0 ; i < end ; i++)
+    {
+        printf("%c ",(unsigned char)buffer[i]);
+    }
+    printf("\n");
+}
+
---
+++ stringarray.cpp
@@ -0,0 +1,24 @@
+#ifndef STRINGARRAY
+#define STRINGARRAY
+
+#include <string.h>
+#include <stdio.h>
+
+// return the char length (size) of an YS_string_array (aircraft list) "s" which cointains "elements" elements (number of aircraft)
+//int sizeofsarray(char* array, int elements)
+//{
+//    int len = 0;
+//    int zeros = 0;
+//    while (zeros<elements)
+//    {
+//        len+=strlen(array+len)+1;
+//        zeros ++;
+//    }
+//    return len;
+//}
+
+void helloworld()
+{
+    printf("Hello World!\n");
+}
+#endif
